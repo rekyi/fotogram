d9c4520bdcc48a257d1c7894b550fbb7e5e72c6e
Rekiya <rekiygemini@gmail.com>
2026-09-02 12:44:38 +0200

    added object array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,53 @@
-let imageArray = ["cat01.webp", "cat02.webp", "cat03.webp", "cat04.webp", "cat05.webp", "cat06.webp", "cat07.webp", "cat08.webp", "cat09.webp", "cat10.webp", "cat11.webp", "cat12.webp", "cat13.webp"];
+let imageArray = [
+  {
+    src: "assets/images/cat01.webp",
+    alt: "Cat and shark having a good time",
+  },
+  {
+    src: "assets/images/cat02.webp",
+    alt: "Cat on a swing",
+  },
+  {
+    src: "assets/images/cat03.webp",
+    alt: "Cat dressed as a wizard",
+  },
+  {
+    src: "assets/images/cat04.webp",
+    alt: "Cat in front of the Eiffel Tower",
+  },
+  {
+    src: "assets/images/cat05.webp",
+    alt: "Cat eating pizza by the Leaning Tower of Pisa",
+  },
+  {
+    src: "assets/images/cat06.webp",
+    alt: "Cat typing on a computer",
+  },
+  {
+    src: "assets/images/cat07.webp",
+    alt: "Cat with headphones drinking Coca-Cola",
+  },
+  {
+    src: "assets/images/cat08.webp",
+    alt: "Cat looks grumpy",
+  },
+  {
+    src: "assets/images/cat09.webp",
+    alt: "Cat at a table",
+  },
+  {
+    src: "assets/images/cat10.webp",
+    alt: "Cat looks frightened",
+  },
+  {
+    src: "assets/images/cat11.webp",
+    alt: "Cat doesn't want to leave",
+  },
+  {
+    src: "assets/images/cat12.webp",
+    alt: "Cats as Michelangelo's Creation of Adam",
+  },
+];
 
 function openDialog(event) {
   const showDialog = event.target.nodeName;
@@ -13,3 +62,5 @@ function openDialog(event) {
     document.getElementById("dialog-opened").showModal();
   }
 }
+
+("");
