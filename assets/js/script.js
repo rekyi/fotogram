02f54ef01fc4e4a4e8534b7e89bd3908b3d6f8fc
Rekiya <rekiygemini@gmail.com>
2026-09-02 12:02:47 +0200

    read src and alt of imgs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-function openDialog(event) {
-  const renderImage = event.target.nodeName;
+let imageArray = ["cat01.webp", "cat02.webp", "cat03.webp", "cat04.webp", "cat05.webp", "cat06.webp", "cat07.webp", "cat08.webp", "cat09.webp", "cat10.webp", "cat11.webp", "cat12.webp", "cat13.webp"];
 
-  if (renderImage === "IMG") {
+function openDialog(event) {
+  const showDialog = event.target.nodeName;
+
+  if (showDialog === "IMG") {
+    document.getElementById("open-dialog").getAttribute("src");
+    document.getElementById("open-dialog").setAttribute("src", "imageArray");
     document.getElementById("dialog-opened").showModal();
   }
 }
