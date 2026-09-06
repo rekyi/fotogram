bdaf11c08efe9ab3cc1020cd7080f7dbcf837227
Rekiya <rekiygemini@gmail.com>
2026-09-06 21:16:03 +0200

    store theme in localStorage

--- assets/js/lightmode.js
+++ assets/js/lightmode.js
@@ -1,9 +1,20 @@
-let themeSwitch = document.querySelectorAll(".theme-toggle-btn");
+let lightMode = localStorage.getItem("lightMode");
+const themeSwitch = document.querySelectorAll(".theme-toggle-btn");
+
+function storeTheme() {
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("lightMode", "active");
+  } else {
+    localStorage.removeItem("lightMode");
+  }
+}
+
 themeSwitch.forEach(function (button) {
   button.addEventListener("click", function () {
     themeSwitch.forEach(function (syncButtons) {
       syncButtons.classList.toggle("active");
     });
     document.body.classList.toggle("light-mode");
+    storeTheme();
   });
 });
