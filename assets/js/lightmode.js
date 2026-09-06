8718f3e64d9ea57ef636a6a9dbad3722aab39c30
Rekiya <rekiygemini@gmail.com>
2026-09-06 22:18:06 +0200

    fixed (f*ck) fouc

--- assets/js/lightmode.js
+++ assets/js/lightmode.js
@@ -1,30 +1,16 @@
-let lightMode = localStorage.getItem("lightMode");
 const themeSwitch = document.querySelectorAll(".theme-toggle-btn");
 
 function storeTheme() {
-  if (document.body.classList.contains("light-mode")) {
+  if (document.documentElement.classList.contains("light-mode")) {
     localStorage.setItem("lightMode", "active");
   } else {
     localStorage.removeItem("lightMode");
   }
 }
 
-function loadTheme() {
-  if (lightMode === "active") {
-    document.body.classList.add("light-mode");
-    themeSwitch.forEach(function (localSync) {
-      localSync.classList.add("active");
-    });
-  }
-}
-
 themeSwitch.forEach(function (button) {
   button.addEventListener("click", function () {
-    themeSwitch.forEach(function (syncButtons) {
-      syncButtons.classList.toggle("active");
-    });
-    document.body.classList.toggle("light-mode");
+    document.documentElement.classList.toggle("light-mode");
     storeTheme();
   });
 });
-loadTheme();
